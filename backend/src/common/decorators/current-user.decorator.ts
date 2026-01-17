50503db54e32e886abdfecc4238cee290c0115ab
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2026-01-17 19:50:52 +0700

    chore: remove unnecessary comment

--- backend/src/common/decorators/current-user.decorator.ts
+++ backend/src/common/decorators/current-user.decorator.ts
@@ -3,6 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<{ user?: any }>();
-    return request.user as { id: string; name: string }; // Replace with the actual expected type
+    return request.user as { id: string; name: string };
   },
 );
